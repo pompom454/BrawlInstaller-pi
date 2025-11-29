8c57d4439b55e2e41934dc8725ec0da395bc27f2
pompom454 <210212892+pompom454@users.noreply.github.com>
2025-11-29 08:10:20 -0700

    did some porting!

--- BrawlInstaller/MainWindow.xaml.cs
+++ BrawlInstaller/MainWindow.xaml.cs
@@ -1,29 +1,11 @@
-﻿using BrawlInstaller.Common;
+﻿using Avalonia.Controls;
+using BrawlInstaller.Common;
 using BrawlInstaller.ViewModels;
 using BrawlLib.SSBB.ResourceNodes;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace BrawlInstaller
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
