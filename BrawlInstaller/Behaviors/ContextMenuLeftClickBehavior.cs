d8d896ac7c5567eb6da0561e2fdbfb4d0e6d85f3
pompom454 <210212892+pompom454@users.noreply.github.com>
2026-02-15 14:56:09 -0700

    Update ContextMenuLeftClickBehavior.cs

--- BrawlInstaller/Behaviors/ContextMenuLeftClickBehavior.cs
+++ BrawlInstaller/Behaviors/ContextMenuLeftClickBehavior.cs
@@ -1,75 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace BrawlInstaller.Behaviors
 {
     public static class ContextMenuLeftClickBehavior
     {
-        public static bool GetIsLeftClickEnabled(DependencyObject obj)
+        public static readonly AttachedProperty<bool>
+            IsLeftClickEnabledProperty =
+            AvaloniaProperty.RegisterAttached<
+                Control,
+                bool>(
+                "IsLeftClickEnabled",
+                typeof(ContextMenuLeftClickBehavior),
+                false);
+
+        public static bool GetIsLeftClickEnabled(Control element)
+            => element.GetValue(IsLeftClickEnabledProperty);
+
+        public static void SetIsLeftClickEnabled(
+            Control element,
+            bool value)
         {
-            return (bool)obj.GetValue(IsLeftClickEnabledProperty);
+            element.SetValue(
+                IsLeftClickEnabledProperty,
+                value);
+
+            if (value)
+                element.PointerReleased += OnPointerReleased;
+            else
+                element.PointerReleased -= OnPointerReleased;
         }
 
-        public static void SetIsLeftClickEnabled(DependencyObject obj, bool value)
+        private static void OnPointerReleased(
+            object sender,
+            PointerReleasedEventArgs e)
         {
-            obj.SetValue(IsLeftClickEnabledProperty, value);
-        }
-
-        public static readonly DependencyProperty IsLeftClickEnabledProperty = DependencyProperty.RegisterAttached(
-            "IsLeftClickEnabled",
-            typeof(bool),
-            typeof(ContextMenuLeftClickBehavior),
-            new UIPropertyMetadata(false, OnIsLeftClickEnabledChanged));
-
-        private static void OnIsLeftClickEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            var uiElement = sender as UIElement;
-
-            if (uiElement != null)
+            if (sender is Control control)
             {
-                bool IsEnabled = e.NewValue is bool && (bool)e.NewValue;
+                if (control.ContextMenu != null)
+                {
+                    if (control.ContextMenu.DataContext == null)
+                    {
+                        control.ContextMenu.DataContext =
+                            control.DataContext;
+                    }
 
-                if (IsEnabled)
-                {
-                    if (uiElement is ButtonBase)
-                        ((ButtonBase)uiElement).Click += OnMouseLeftButtonUp;
-                    else
-                        uiElement.MouseLeftButtonUp += OnMouseLeftButtonUp;
-                }
-                else
-                {
-                    if (uiElement is ButtonBase)
-                        ((ButtonBase)uiElement).Click -= OnMouseLeftButtonUp;
-                    else
-                        uiElement.MouseLeftButtonUp -= OnMouseLeftButtonUp;
+                    control.ContextMenu.Open(control);
                 }
             }
         }
-
-        private static void OnMouseLeftButtonUp(object sender, RoutedEventArgs e)
-        {
-            Debug.Print("OnMouseLeftButtonUp");
-            var fe = sender as FrameworkElement;
-            if (fe != null)
-            {
-                // if we use binding in our context menu, then it's DataContext won't be set when we show the menu on left click
-                // (it seems setting DataContext for ContextMenu is hardcoded in WPF when user right clicks on a control, although I'm not sure)
-                // so we have to set up ContextMenu.DataContext manually here
-                if (fe.ContextMenu.DataContext == null)
-                {
-                    fe.ContextMenu.SetBinding(FrameworkElement.DataContextProperty, new Binding { Source = fe.DataContext });
-                }
-
-                fe.ContextMenu.IsOpen = true;
-            }
-        }
-
     }
 }
