c394a0a87f82e885ee3ed826c0acc32e5d42cc1c
pompom454 <210212892+pompom454@users.noreply.github.com>
2026-02-15 18:45:14 -0700

    Create Label.cs

---
+++ lavaKirbyHatManagerV2/Sys/Label.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace System.Windows.Forms
+{
+    public class Label : TextBlock
+    {
+        public object AutoSize { get; set; }
+        public object Font { get; set; }
+        public object Location { get; set; }
+        public object Size { get; set; }
+        public object TextAlign { get; set; }
+    }
+}
