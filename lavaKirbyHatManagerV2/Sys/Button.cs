ac1ee500d17e3b4af51c52ac425e90bf9d87a266
pompom454 <210212892+pompom454@users.noreply.github.com>
2026-02-15 18:44:49 -0700

    Create Button.cs

---
+++ lavaKirbyHatManagerV2/Sys/Button.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace System.Windows.Forms
+{
+    public class Button : Avalonia.Controls.Button
+    {
+        public object DialogResult { get; set; }
+        public object UseVisualStyleBackColor { get; set; }
+    }
+}
